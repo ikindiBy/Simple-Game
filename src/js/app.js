d0836b923b0370cf51ce065868f3c6e7ea1ad293
ikindiBy <ikindi@mail.ru>
2017-12-03 13:56:25 +0300

    drawing/draw hero

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 
 import SourceSprites from './SourceSprites.js';
 import {loadImage, loadDataFromJSON} from './loaders.js';
-
+import {loadBacgroundSprites, loadHeroSprites} from './sprites.js';
 	
 function drawBackground(backgrounOf, context, sprites) {
 		backgrounOf.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -14,7 +14,6 @@ function drawBackground(backgrounOf, context, sprites) {
 	})
 }
 
-
 	const canvas = document.getElementById('canvas');
 
 	canvas.setAttribute('width', 960);
@@ -22,21 +21,36 @@ function drawBackground(backgrounOf, context, sprites) {
 
 	const context = canvas.getContext('2d');
 
-	loadImage('../images/sprites.png')
+
+/*   //first version of drawing bgc!
+loadImage('../images/sprites.png')
 	.then(image => {
 		const sprites = new SourceSprites(image);
 
 		loadDataFromJSON('sprites')
-		.then( fileJSON => {
-			sprites.defineTilePerJSON(fileJSON, 'box-g.png');
-			sprites.defineTilePerJSON(fileJSON, 'box-b1.png');
+		.then( spritesDataFromJSON => {
+			sprites.defineTilePerJSON(spritesDataFromJSON, 'box-g.png');
+			sprites.defineTilePerJSON(spritesDataFromJSON, 'box-b1.png');
 
 			loadDataFromJSON('level-1')
-			.then( fileJSON => {
-				fileJSON.backgrounds.forEach(background => {
+			.then( levelDataFromJSON => {
+				levelDataFromJSON.backgrounds.forEach(background => {
 					drawBackground(background, context, sprites);
 				});
 			});
 		});
 	});
-	
+	*/
+
+Promise.all([
+	loadHeroSprites(),
+	loadBacgroundSprites(),
+	loadDataFromJSON('level-1')
+	])
+	.then(([hero, sprites, levelDataFromJSON]) => {
+		levelDataFromJSON.backgrounds.forEach(background => {
+		drawBackground(background, context, sprites);
+		});
+
+		hero.drawTile('boy3.png', context, 200, 520);
+	})
---
+++ src/js/sprites.js
@@ -0,0 +1,33 @@
+import SourceSprites from './SourceSprites.js';
+import {loadImage, loadDataFromJSON} from './loaders.js';
+
+export function loadBacgroundSprites() {
+	return loadImage('../images/sprites.png')
+	.then(image => {
+		const sprites = new SourceSprites(image);
+		return sprites ;
+	})
+	.then(sprites => {
+		return loadDataFromJSON('sprites')
+		.then( spritesDataFromJSON => {
+			sprites.defineTilePerJSON(spritesDataFromJSON, 'box-g.png');
+			sprites.defineTilePerJSON(spritesDataFromJSON, 'box-b1.png');
+			return sprites ;
+		});
+	});
+}
+
+export function loadHeroSprites() {
+	return loadImage('../images/sprites.png')
+	.then(image => {
+		const sprites = new SourceSprites(image);
+		return sprites ;
+	})
+	.then(sprites => {
+		return loadDataFromJSON('sprites')
+		.then( spritesDataFromJSON => {
+			sprites.defineTilePerJSON(spritesDataFromJSON, 'boy3.png');
+			return sprites ;
+		});
+	});
+}
