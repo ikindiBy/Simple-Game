b8ecb3a679dc5878fc53ea874623622d9491eede
chelovekdrakon <MorauFiodar@yahoo.com>
2017-12-03 13:27:09 +0300

    Game-field/feature: Spritesheet class have been expended to be able to store all images(define() -> this.tiles). Draw() method now is drawing by simply calling a name + position

--- src/game-field.js
+++ src/game-field.js
@@ -1,28 +1,18 @@
-import spriteSheet from './spritesheet';
+import Spritesheet from './spritesheet';
 import {loadImage,loadJSON} from './loaders';
 
 export default function drawField(context, canvas) {
 
     loadImage('./images/sprites.png').then(image => {
 
-        loadJSON('./sprites').then( sprite => {
-            const sprites = new spriteSheet(image, sprite);
-            // sprites.draw(context, `sky3.png`, 0, 0);
-            // const allImg = [];
-            // for (let i in sprites.data) {
-            //     allImg.push(i);
-            // }
-            // allImg.forEach( (img, index) => {
-            //     sprites.draw(context, `${img}`, index*20, index*20);
-            // });
-            for (let i = 0; i < canvas.width; ) {
-                sprites.draw(context, `box-b.png`, i, canvas.height - 37);
-                i += 37;
+        loadJSON('./sprites').then( data => {
+            const sprites = new Spritesheet(image, data);
+
+            for (let i in data) {
+                sprites.define(i)
             }
-            for (let i = 0; i < canvas.width; ) {
-                sprites.draw(context, `box-gb1.png`, i, canvas.height - 74);
-                i += 37;
-            }
+
+            sprites.draw(`box-b`, context, 0, 0);
         });
     });
 }
--- src/spritesheet.js
+++ src/spritesheet.js
@@ -1,11 +1,25 @@
-export default class spriteSheet {
+export default class Spritesheet {
     constructor(image, data) {
         this.image = image;
         this.data = data;
+        this.tiles = new Map();
     }
-    draw(context, name, posx, posy){
+    define(name) {
+        const buffer = document.createElement('canvas');
+        const context = buffer.getContext('2d');
+
         const png = this.data[`${name}`].frame;
+
+        buffer.width = png['w'];
+        buffer.height =png['h'];
+
         context.drawImage(this.image, png['x'], png['y'], png['w'], png['h'],
-                                      posx,     posy,     png['w'], png['h']);
+                                      0,        0,        png['w'], png['h']);
+
+        this.tiles.set(name, buffer);
+    }
+    draw(name, context, x, y){
+        const buffer = this.tiles.get(`${name}.png`);
+        context.drawImage(buffer, x, y);
     }
 }
