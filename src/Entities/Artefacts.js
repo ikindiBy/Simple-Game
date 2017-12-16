b0b00d2a8d899df33a5bd8e527ce96eb3f258669
ikindiBy <ikindi@mail.ru>
2017-12-17 00:26:14 +0300

    locks/ add naturalistic moving for flying enemy

---
+++ src/Entities/Artefacts.js
@@ -0,0 +1,48 @@
+import Entity, {Sides, Trait} from '../Entity';
+import Killable from '../Traits/Killable';
+
+class Behavior extends Trait{
+    constructor() {
+        super('behavior');
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+
+        if (them.stomper) {
+            us.killable.removeAfter = 0;
+            
+            if (us.name.includes('coin')) {
+                us.killable.kill();
+                them.stateCosmo.coins++;
+            } else if (us.name.includes('key')) {
+                us.killable.kill();
+                them.stateCosmo.keys++;
+            } else if (us.name.includes('lock')) {
+                if (them.stateCosmo.keys) {
+                    us.killable.kill();
+                    console.log('congratulation - next level');
+                }
+            }
+
+        }
+    }
+}
+
+export function createArtefactsFactory(sprites) {
+
+    return function createArtefact(name, posX, posY) {
+
+        const artefact = new Entity(name);
+        artefact.size.set(23, 23);               //!!! resolve
+        artefact.pos.set(posX * 37, posY * 37);  //!!! resolve
+        artefact.offset.set(7, 7);
+        artefact.picture = name;
+
+        artefact.addTrait(new Killable());
+        artefact.addTrait(new Behavior());
+        sprites.entities.add(artefact);
+    };
+}
---
+++ src/Entities/Enemies.js
@@ -0,0 +1,66 @@
+import Entity, {Sides, Trait} from '../Entity';
+import Jump from '../Traits/Jump';
+import Go from '../Traits/Go';
+import Killable from '../Traits/Killable';
+import PendulumWalk from '../Traits/PendulumWalk';
+import Physics from '../Traits/Physics';
+
+class Behavior extends Trait{
+    constructor() {
+        super('behavior');
+    }
+
+    reachEdge(enemy) {
+
+        console.log(enemy.pendulumWalk.speed);
+
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                us.pendulumWalk.speed = 0;
+                us.killable.kill();
+                them.stomper.bounce();
+            } else {
+                them.killable.kill();
+            }
+        }
+    }
+
+    update(entity, deltaTime, sprites) {
+        const sequence = Math.floor(entity.lifetime % entity.pictures.length);
+        entity.picture = entity.pictures[sequence];
+    }
+}
+
+export function createEnemiesFactory(sprites) {
+
+    return function createEnemy(name, posX, posY,
+                                      offsetX, offsetY,
+                                      sizeX, sizeY,
+                                      velX, velY,
+                                      reverse, pictures, deadPic) {
+
+        const enemy = new Entity(name);
+        enemy.size.set(sizeX, sizeY);
+        enemy.pos.set(posX * 37, posY * 37);
+        enemy.startPositin.set(posX * 37, posY * 37);
+        enemy.vel.set(velX, velY);
+        enemy.offset.set(offsetX, offsetY);
+        enemy.pictures = reverse ? Array.from(pictures).reverse() : pictures;
+        enemy.picture = null;
+        enemy.deadPic = deadPic;
+
+        enemy.addTrait(new Physics());
+        enemy.addTrait(new PendulumWalk(reverse));
+        enemy.addTrait(new Behavior());
+        enemy.addTrait(new Killable());
+
+        sprites.entities.add(enemy);
+    };
+}
