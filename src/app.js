15d34c703e0a6aa3bf309b2d4fbd9b30e1a7e72d
chelovekdrakon <MorauFiodar@yahoo.com>
2017-12-17 13:10:11 +0300

    Menu/feature: Had added ability to start the game by pressing Enter

--- src/app.js
+++ src/app.js
@@ -1,13 +1,48 @@
 import drawField from './game-field';
+import {loadImage,loadJSON} from './loaders';
+import Sprite from './Sprite';
+import Font from './Font';
+import createLevelCompositor from './createLevelCompositor';
+import drawStartMenu from './drawStartMenu';
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-// canvas.setAttribute('width', `${window.devicePixelRatio}` * 960);
-// canvas.setAttribute('height', `${window.devicePixelRatio}` * 640);
-// canvas.style.width = `960px`;
-// canvas.style.height = `640px`;
-
 canvas.setAttribute('width', 960);
 canvas.setAttribute('height', 629);
 
-drawField(context, canvas);
+const game = Promise.all([
+    loadImage('./images/sprites.png'),
+    loadJSON('./sprites'),
+    loadJSON('./alphabet'),
+    loadImage('./images/alphabet.png')
+])
+.then(async ([tileImage, tileData, fontData, fontImage]) => {
+
+    const sprites = new Sprite(tileImage, tileData);
+    for (let sprite in tileData) {
+        sprites.define(sprite);
+    }
+    sprites.drawLevel = await createLevelCompositor(sprites);
+
+    const font = new Font(fontImage, fontData);
+
+    for (let letter in fontData) {
+        font.define(letter);
+    }
+
+    drawStartMenu(context, sprites, font);
+
+    return [sprites, font];
+})
+
+const start = function(event) {
+    if (event.key === 'Enter') {
+        window.removeEventListener('keypress', start);
+
+        game.then(([sprites, font]) => {
+            drawField(context, sprites);
+        })
+    }
+}
+
+window.addEventListener('keypress', start);
--- src/game-field.js
+++ src/game-field.js
@@ -1,80 +1,25 @@
-import Sprite from './Sprite';
-import Font from './Font';
-import {loadImage,loadJSON} from './loaders';
 import Timer from './timer';
 
 import createCollisionLayer from './layers/collision';
 import createCameraLayer from './layers/camera';
 
-import createLevelCompositor from './createLevelCompositor';
 import createPlayerEnvironment from './createPlayerEnvironment';
 
-export default function drawField(context, canvas) {
-    Promise.all([
-        loadImage('./images/sprites.png'),
-        loadJSON('./sprites'),
-        loadJSON('./alphabet'),
-        loadImage('./images/alphabet.png')
-    ])
-    .then(async ([tileImage, tileData, fontData, fontImage]) => {
+export default async function drawField(context, sprites) {
 
+    const drawCollisions = createCollisionLayer(sprites);
+    const drawCameraView = createCameraLayer(sprites.camera);
 
-        const sprites = new Sprite(tileImage, tileData);
-        for (let sprite in tileData) {
-            sprites.define(sprite);
-        }
+    const [drawDashboard, cosmo] = createPlayerEnvironment(sprites);
 
-        // const font = new Font(fontsData, fontData);
-        // 
-        // for (let letter in fontData) {
-        //     font.define(letter);
-        // }
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
 
-        const drawCollisions = createCollisionLayer(sprites);
-        const drawCameraView = createCameraLayer(sprites.camera);
+        sprites.drawLevel(context);
+        sprites.update(deltaTime, context);
 
-        const [drawDashboard, cosmo] = createPlayerEnvironment(sprites);
-        sprites.drawLevel = await createLevelCompositor(sprites);
-
-        // let loadSound = function(url, callback) {
-        //     console.log('loadSound', url)
-        //     let loaded = function () {
-        //         callback(sound);
-        //         sound.removeEventListener("canplaythrough", loaded);
-        //     }
-        //     let sound = new Audio (url);
-        //     sound.addEventListener("canplaythrough", loaded);
-        //     sound.load();
-        // }
-
-        // let self = this;
-
-        // loadSound('src/sound.mp3', function(mySound) {
-        //     self.mySound = mySound;
-
-        // });
-
-        // this.mySound.load();
-        // this.mySound.play();
-
-        // let sound = new Audio ('src/sound.mp3');
-
-        // sound.load();
-        // sound.play();
-
-
-
-        const timer = new Timer(1/60);
-        timer.update = function update(deltaTime) {
-
-                sprites.drawLevel(context);
-                sprites.update(deltaTime, context);
-
-                drawCollisions(context, sprites.camera);
-                drawCameraView(context, sprites.camera);
-
-                drawDashboard(context);
-        }
-        timer.start();
-    })
+        drawCollisions(context, sprites.camera);
+        drawCameraView(context, sprites.camera);
+    }
+    timer.start();
 }
